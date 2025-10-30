1403ec65b71cba891136232b82f08a0af6052916
Diego Diomir Mamani Ramos <drmamani@entel.bo>
2025-10-30 16:39:16 -0400

    refactor: Footer con iconos agregado

--- ges-mobile-f/src/app/layout/component/footer.ts
+++ ges-mobile-f/src/app/layout/component/footer.ts
@@ -4,26 +4,28 @@ import { Component } from '@angular/core';
   selector: 'app-footer',
   imports: [],
   template: `
-  <footer class="fixed bottom-0 w-full z-0 md:z-50 bg-white rounded-lg shadow-sm dark:bg-gray-800">
-      <div class="w-full mx-auto p-4 md:flex md:items-center md:justify-between">
-        <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" class="hover:underline">Flowbite™</a>. All Rights Reserved.
-      </span>
-      <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
-          <li>
-              <a href="#" class="hover:underline me-4 md:me-6">About</a>
-          </li>
-          <li>
-              <a href="#" class="hover:underline me-4 md:me-6">Privacy Policy</a>
-          </li>
-          <li>
-              <a href="#" class="hover:underline me-4 md:me-6">Licensing</a>
-          </li>
-          <li>
-              <a href="#" class="hover:underline">Contact</a>
-          </li>
-      </ul>
-      </div>
-  </footer>
+    <footer class="fixed bottom-0 w-full z-0 md:z-50 bg-white rounded-t-2xl shadow-sm dark:bg-gray-800">
+        <div class="w-full mx-auto p-4">
+            <ul class="flex justify-around gap-2">
+                <li class="p-1 px-2 hover:cursor-pointer">
+                    <svg class="w-6 h-6 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m4 12 8-8 8 8M6 10.5V19a1 1 0 0 0 1 1h3v-3a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3h3a1 1 0 0 0 1-1v-8.5"/>
+                    </svg>
+                </li>
+                <li class="p-1 px-2 hover:cursor-pointer">
+                    <svg class="w-6 h-6 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 4v15a1 1 0 0 0 1 1h15M8 16l2.5-5.5 3 3L17.273 7 20 9.667"/>
+                    </svg>
+                </li>
+                <li class="p-1 px-2 hover:cursor-pointer">
+                    <svg class="w-6 h-6 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 13v-2a1 1 0 0 0-1-1h-.757l-.707-1.707.535-.536a1 1 0 0 0 0-1.414l-1.414-1.414a1 1 0 0 0-1.414 0l-.536.535L14 4.757V4a1 1 0 0 0-1-1h-2a1 1 0 0 0-1 1v.757l-1.707.707-.536-.535a1 1 0 0 0-1.414 0L4.929 6.343a1 1 0 0 0 0 1.414l.536.536L4.757 10H4a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h.757l.707 1.707-.535.536a1 1 0 0 0 0 1.414l1.414 1.414a1 1 0 0 0 1.414 0l.536-.535 1.707.707V20a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1v-.757l1.707-.708.536.536a1 1 0 0 0 1.414 0l1.414-1.414a1 1 0 0 0 0-1.414l-.535-.536.707-1.707H20a1 1 0 0 0 1-1Z"/>
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 15a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"/>
+                    </svg>
+                </li>
+            </ul>
+        </div>
+    </footer>
   `,
 })
 export class Footer {

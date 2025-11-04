e1a4bd6e66bacf7f1e75a6206d18b5bfbb622bea
Diego Diomir <aries-d2github@hotmail.com>
2025-11-04 14:20:59 -0400

    feat: Opciones principales del sidebar

--- ges-mobile-f/src/app/layout/component/sidebar.ts
+++ ges-mobile-f/src/app/layout/component/sidebar.ts
@@ -130,34 +130,6 @@ import { Component } from '@angular/core';
           </li>
 
           <li>
-            <a
-              href="#"
-              class="flex items-center p-2 text-base font-medium text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
-            >
-              <svg
-                aria-hidden="true"
-                class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M8.707 7.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l2-2a1 1 0 00-1.414-1.414L11 7.586V3a1 1 0 10-2 0v4.586l-.293-.293z"
-                ></path>
-                <path
-                  d="M3 5a2 2 0 012-2h1a1 1 0 010 2H5v7h2l1 2h4l1-2h2V5h-1a1 1 0 110-2h1a2 2 0 012 2v10a2 2 0 01-2 2H5a2 2 0 01-2-2V5z"
-                ></path>
-              </svg>
-              <span class="flex-1 ml-3 whitespace-nowrap">Messages</span>
-              <span
-                class="inline-flex justify-center items-center w-5 h-5 text-xs font-semibold rounded-full text-primary-800 bg-primary-100 dark:bg-primary-200 dark:text-primary-800"
-              >
-                4
-              </span>
-            </a>
-          </li>
-
-          <li>
             <button
               type="button"
               class="flex items-center p-2 w-full text-base font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
@@ -177,9 +149,7 @@ import { Component } from '@angular/core';
                   clip-rule="evenodd"
                 ></path>
               </svg>
-              <span class="flex-1 ml-3 text-left whitespace-nowrap"
-              >Authentication</span
-              >
+              <span class="flex-1 ml-3 text-left whitespace-nowrap">Historiales</span>
               <svg
                 aria-hidden="true"
                 class="w-6 h-6"
@@ -199,22 +169,21 @@ import { Component } from '@angular/core';
                 <a
                   href="#"
                   class="flex items-center p-2 pl-11 w-full text-base font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
-                >Sign In</a
                 >
+                  Historial de recargas prepago
+                </a>
               </li>
               <li>
                 <a
                   href="#"
                   class="flex items-center p-2 pl-11 w-full text-base font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
-                >Sign Up</a
-                >
+                >Historial de recargas postpago</a>
               </li>
               <li>
                 <a
                   href="#"
                   class="flex items-center p-2 pl-11 w-full text-base font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
-                >Forgot Password</a
-                >
+                >Hisotial de compras</a>
               </li>
             </ul>
           </li>
--- ges-mobile-f/src/app/layout/layout.ts
+++ ges-mobile-f/src/app/layout/layout.ts
@@ -217,26 +217,6 @@ import { Footer } from "./component/footer";
 
         </div>
 
-
-
-        <div class="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-96 mb-4"></div>
-
-        <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 mb-4">
-          <div class="border-2 border-dashed rounded-4xl border-gray-300 dark:border-gray-600 h-32 md:h-64"></div>
-          <div class="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-32 md:h-64"></div>
-          <div class="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-32 md:h-64"></div>
-          <div class="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-32 md:h-64"></div>
-        </div>
-
-        <div class="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-96 mb-4"></div>
-
-        <div class="grid grid-cols-2 gap-4">
-          <div class="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-48 md:h-72"></div>
-          <div class="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-48 md:h-72"></div>
-          <div class="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-48 md:h-72"></div>
-          <div class="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-48 md:h-72"></div>
-        </div>
-
       </main>
       <app-footer></app-footer>
     </div>

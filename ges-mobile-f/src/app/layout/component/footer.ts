d6c6d13092cdaf233ef9b55d567fd20c00f23d4d
Diegoxyz <aries-d2github@hotmail.com>
2025-11-06 14:36:47 -0400

    feat: Boton Home navegacion al dashboard principal

--- ges-mobile-f/src/app/layout/component/footer.ts
+++ ges-mobile-f/src/app/layout/component/footer.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   template: `
     <footer class="fixed bottom-0 w-full z-0 md:z-50 bg-white rounded-t-2xl shadow-sm dark:bg-gray-800">
         <div class="w-full mx-auto p-4">
             <ul class="flex justify-around gap-2">
-                <li class="p-1 px-2 hover:cursor-pointer">
+                <li routerLink="/" class="p-1 px-2 hover:cursor-pointer">
                     <svg class="w-6 h-6 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m4 12 8-8 8 8M6 10.5V19a1 1 0 0 0 1 1h3v-3a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3h3a1 1 0 0 0 1-1v-8.5"/>
                     </svg>
